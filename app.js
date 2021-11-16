2657059c5d3e9986da163487b97317a5794055be
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-11-16 22:58:44 +0530

    done

---
+++ app.js
@@ -0,0 +1,15 @@
+const gayab=document.querySelector("#gayab");
+const btn=document.querySelector("#btn");
+
+btn.addEventListener("click",dijappear);
+
+function dijappear(){
+gayab.style.display = "none";
+}
+// delay();
+//       setTimeout(function(){ output.innerText = `The nearest palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you need to wait for ${ctr} days.`; }, 1500);
+
+//     function delay()
+//  {
+//   setTimeout(function(){ output.innerText = "Calculating.....   ∫eˣ - log(x)..."; }, 1);
+//  }
